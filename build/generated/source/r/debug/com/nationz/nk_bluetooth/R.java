6461969b6713309322485d2300bef60b29957296
251820389 <251820389@qq.com>
2016-08-22 17:06:03 +0800

    改动点
    交易so库初步调用测试

--- build/generated/source/r/debug/com/nationz/nk_bluetooth/R.java
+++ build/generated/source/r/debug/com/nationz/nk_bluetooth/R.java
@@ -12,6 +12,110 @@ public final class R {
         public static final int array_setting=0x7f060000;
     }
     public static final class attr {
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int pivBorderColor=0x7f010000;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int pivBorderRadius=0x7f010002;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int pivBorderWidth=0x7f010001;
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int pivPasswordColor=0x7f010003;
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int pivPasswordLength=0x7f010006;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int pivPasswordRadius=0x7f010005;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int pivPasswordWidth=0x7f010004;
+    }
+    public static final class color {
+        public static final int bg_color=0x7f070000;
+        public static final int black=0x7f070001;
+        public static final int contents_text=0x7f070002;
+        public static final int dialog_btn=0x7f070003;
+        public static final int encode_view=0x7f070004;
+        public static final int gray=0x7f070005;
+        public static final int grgray=0x7f070006;
+        public static final int gridview_item=0x7f070007;
+        public static final int header=0x7f070008;
+        public static final int help_button_view=0x7f070009;
+        public static final int help_view=0x7f07000a;
+        public static final int icon=0x7f07000b;
+        public static final int listview_item=0x7f07000c;
+        public static final int possible_result_points=0x7f07000d;
+        public static final int result_image_border=0x7f07000e;
+        public static final int result_minor_text=0x7f07000f;
+        public static final int result_points=0x7f070010;
+        public static final int result_text=0x7f070011;
+        public static final int result_view=0x7f070012;
+        public static final int sbc_header_text=0x7f070013;
+        public static final int sbc_header_view=0x7f070014;
+        public static final int sbc_layout_view=0x7f070015;
+        public static final int sbc_list_item=0x7f070016;
+        public static final int sbc_page_number_text=0x7f070017;
+        public static final int sbc_snippet_text=0x7f070018;
+        public static final int share_text=0x7f070019;
+        public static final int share_view=0x7f07001a;
+        public static final int status_text=0x7f07001b;
+        public static final int status_view=0x7f07001c;
+        public static final int text_color=0x7f07001d;
+        public static final int transparent=0x7f07001e;
+        public static final int view_line=0x7f07001f;
+        public static final int viewfinder_frame=0x7f070020;
+        public static final int viewfinder_laser=0x7f070021;
+        public static final int viewfinder_mask=0x7f070022;
+        public static final int white=0x7f070023;
     }
     public static final class dimen {
         public static final int activity_horizontal_margin=0x7f040000;
@@ -21,42 +125,45 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090023;
-        public static final int address=0x7f090015;
-        public static final int backBtn=0x7f090008;
-        public static final int classicRB=0x7f090003;
-        public static final int classicZone=0x7f09000c;
-        public static final int conBtn=0x7f090009;
-        public static final int dataMountTV=0x7f09001b;
-        public static final int dataNumTV=0x7f090010;
-        public static final int dataSpeedTV=0x7f09001c;
-        public static final int dataTV=0x7f090012;
-        public static final int devAdress=0x7f09000b;
-        public static final int devName=0x7f09000a;
-        public static final int fragmentLayout=0x7f090007;
-        public static final int gatt_services_list=0x7f090013;
-        public static final int leRB=0x7f090004;
-        public static final int menu_connect=0x7f090021;
-        public static final int menu_disconnect=0x7f090022;
-        public static final int menu_refresh=0x7f090020;
-        public static final int name=0x7f090014;
-        public static final int opBTN=0x7f090019;
-        public static final int propTV=0x7f090016;
-        public static final int readPropBTN=0x7f090018;
-        public static final int revLayout=0x7f09001a;
-        public static final int rg=0x7f090002;
-        public static final int scanBtn=0x7f090005;
-        public static final int sendBtn=0x7f09000e;
-        public static final int sendData=0x7f09000f;
-        public static final int sendET=0x7f09001e;
-        public static final int sendLayout=0x7f09001d;
-        public static final int sendTimesET=0x7f09001f;
-        public static final int speedTV=0x7f090011;
-        public static final int spinner=0x7f09000d;
-        public static final int tv=0x7f090000;
-        public static final int tv2=0x7f090001;
-        public static final int uuidTV=0x7f090017;
-        public static final int visibleBtn=0x7f090006;
+        public static final int action_settings=0x7f0a0026;
+        public static final int address=0x7f0a0015;
+        public static final int backBtn=0x7f0a0008;
+        public static final int classicRB=0x7f0a0003;
+        public static final int classicZone=0x7f0a000c;
+        public static final int conBtn=0x7f0a0009;
+        public static final int dataMountTV=0x7f0a001b;
+        public static final int dataNumTV=0x7f0a0010;
+        public static final int dataSpeedTV=0x7f0a001c;
+        public static final int dataTV=0x7f0a0012;
+        public static final int devAdress=0x7f0a000b;
+        public static final int devName=0x7f0a000a;
+        public static final int fragmentLayout=0x7f0a0007;
+        public static final int gatt_services_list=0x7f0a0013;
+        public static final int leRB=0x7f0a0004;
+        public static final int menu_connect=0x7f0a0024;
+        public static final int menu_disconnect=0x7f0a0025;
+        public static final int menu_refresh=0x7f0a0023;
+        public static final int name=0x7f0a0014;
+        public static final int opBTN=0x7f0a0019;
+        public static final int passwordInputView=0x7f0a0022;
+        public static final int propTV=0x7f0a0016;
+        public static final int pwdInputView=0x7f0a0020;
+        public static final int pwd_btn=0x7f0a0021;
+        public static final int readPropBTN=0x7f0a0018;
+        public static final int revLayout=0x7f0a001a;
+        public static final int rg=0x7f0a0002;
+        public static final int scanBtn=0x7f0a0005;
+        public static final int sendBtn=0x7f0a000e;
+        public static final int sendData=0x7f0a000f;
+        public static final int sendET=0x7f0a001e;
+        public static final int sendLayout=0x7f0a001d;
+        public static final int sendTimesET=0x7f0a001f;
+        public static final int speedTV=0x7f0a0011;
+        public static final int spinner=0x7f0a000d;
+        public static final int tv=0x7f0a0000;
+        public static final int tv2=0x7f0a0001;
+        public static final int uuidTV=0x7f0a0017;
+        public static final int visibleBtn=0x7f0a0006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -64,17 +171,22 @@ public final class R {
         public static final int device_list=0x7f030002;
         public static final int devicelist_fragment=0x7f030003;
         public static final int gatt_layout=0x7f030004;
+        public static final int pwd_input_layout=0x7f030005;
+        public static final int test_layout=0x7f030006;
     }
     public static final class menu {
-        public static final int gatt_services=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int gatt_services=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f070000;
-        public static final int app_name=0x7f070001;
-        public static final int hello_world=0x7f070002;
-        public static final int unknown_characteristic=0x7f070003;
-        public static final int unknown_service=0x7f070004;
+        public static final int action_settings=0x7f080000;
+        public static final int app_name=0x7f080001;
+        public static final int cancel_trade=0x7f080002;
+        public static final int error_pwd_len=0x7f080003;
+        public static final int hello_world=0x7f080004;
+        public static final int submit=0x7f080005;
+        public static final int unknown_characteristic=0x7f080006;
+        public static final int unknown_service=0x7f080007;
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
@@ -90,4 +202,140 @@ public final class R {
          */
         public static final int AppTheme=0x7f050001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a PasswordInputView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #PasswordInputView_pivBorderColor com.nationz.nk_bluetooth:pivBorderColor}</code></td><td></td></tr>
+           <tr><td><code>{@link #PasswordInputView_pivBorderRadius com.nationz.nk_bluetooth:pivBorderRadius}</code></td><td></td></tr>
+           <tr><td><code>{@link #PasswordInputView_pivBorderWidth com.nationz.nk_bluetooth:pivBorderWidth}</code></td><td></td></tr>
+           <tr><td><code>{@link #PasswordInputView_pivPasswordColor com.nationz.nk_bluetooth:pivPasswordColor}</code></td><td></td></tr>
+           <tr><td><code>{@link #PasswordInputView_pivPasswordLength com.nationz.nk_bluetooth:pivPasswordLength}</code></td><td></td></tr>
+           <tr><td><code>{@link #PasswordInputView_pivPasswordRadius com.nationz.nk_bluetooth:pivPasswordRadius}</code></td><td></td></tr>
+           <tr><td><code>{@link #PasswordInputView_pivPasswordWidth com.nationz.nk_bluetooth:pivPasswordWidth}</code></td><td></td></tr>
+           </table>
+           @see #PasswordInputView_pivBorderColor
+           @see #PasswordInputView_pivBorderRadius
+           @see #PasswordInputView_pivBorderWidth
+           @see #PasswordInputView_pivPasswordColor
+           @see #PasswordInputView_pivPasswordLength
+           @see #PasswordInputView_pivPasswordRadius
+           @see #PasswordInputView_pivPasswordWidth
+         */
+        public static final int[] PasswordInputView = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003,
+            0x7f010004, 0x7f010005, 0x7f010006
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.nationz.nk_bluetooth.R.attr#pivBorderColor}
+          attribute's value can be found in the {@link #PasswordInputView} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.nationz.nk_bluetooth:pivBorderColor
+        */
+        public static final int PasswordInputView_pivBorderColor = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.nationz.nk_bluetooth.R.attr#pivBorderRadius}
+          attribute's value can be found in the {@link #PasswordInputView} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.nationz.nk_bluetooth:pivBorderRadius
+        */
+        public static final int PasswordInputView_pivBorderRadius = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.nationz.nk_bluetooth.R.attr#pivBorderWidth}
+          attribute's value can be found in the {@link #PasswordInputView} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.nationz.nk_bluetooth:pivBorderWidth
+        */
+        public static final int PasswordInputView_pivBorderWidth = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.nationz.nk_bluetooth.R.attr#pivPasswordColor}
+          attribute's value can be found in the {@link #PasswordInputView} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.nationz.nk_bluetooth:pivPasswordColor
+        */
+        public static final int PasswordInputView_pivPasswordColor = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.nationz.nk_bluetooth.R.attr#pivPasswordLength}
+          attribute's value can be found in the {@link #PasswordInputView} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.nationz.nk_bluetooth:pivPasswordLength
+        */
+        public static final int PasswordInputView_pivPasswordLength = 6;
+        /**
+          <p>This symbol is the offset where the {@link com.nationz.nk_bluetooth.R.attr#pivPasswordRadius}
+          attribute's value can be found in the {@link #PasswordInputView} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.nationz.nk_bluetooth:pivPasswordRadius
+        */
+        public static final int PasswordInputView_pivPasswordRadius = 5;
+        /**
+          <p>This symbol is the offset where the {@link com.nationz.nk_bluetooth.R.attr#pivPasswordWidth}
+          attribute's value can be found in the {@link #PasswordInputView} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.nationz.nk_bluetooth:pivPasswordWidth
+        */
+        public static final int PasswordInputView_pivPasswordWidth = 4;
+    };
 }
